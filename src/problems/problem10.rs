a74f75e74b691dee57c3fef70d94c5e70fa69f78
kzhang425 <105956325+kzhang425@users.noreply.github.com>
2022-10-27 14:05:33 -0400

    optimized

--- src/problems/problem10.rs
+++ src/problems/problem10.rs
@@ -1,29 +1,16 @@
 // We can reuse a function defined in problem 7, helps us check prime in a vector of vectors in order
-use super::problem7::check_prime;
+use super::useful_functions::e_sieve;
 
 pub fn solve() -> (String, u64) {
     let problem_statement = String::from(
         "Find the sum of the primes below two million. This may take a long time."
     );
 
-    let mut primes: Vec<u32> = Vec::new();
-    primes.push(2);
-    
-    // Initialize the counter that will start counting up. For more speed, only
-    // consider odd numbers
-    let upper_lim = 2000000;
-    let mut counter: u32 = 3;
-    while counter < upper_lim {
-        if check_prime(counter, &primes) {
-            primes.push(counter);
-        }
-        counter += 2;
-    }
+    let primes = e_sieve(2000000);
 
-    let mut sum = 0;
-    for num in primes {
-        sum += num as u64;
-    }
+    let sum = primes.iter()
+        .map(|&num| num as u64)
+        .sum();
 
     (problem_statement, sum)
 
